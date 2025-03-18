b4d6b996b16b455209c84a7311fd1565218e2b74
Chandana P <chandana6323@gmail.com>
2025-03-18 18:07:58 +0530

    Initialize Cypress fixtures with example,and user data

--- zidio-react/src/App.js
+++ zidio-react/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavbarHeader from "./components/NavbarHeader/NavbarHeader";
+import Client from "./components/Client/Client";
+import Project from "./components/Project/Project";
+import Employees from "./components/Employees";
+import ChangePassword from "./components/Login/ChangePassword";
+import Home from "./components/Home";
+import AddExpenseSheet from "./components/Expense/AddExpenseSheet";
+import ProjectSummary from "./components/Project/ProjectSummary";
+import Approvals from "./components/Approval/Approvals";
+import AddEmployee from "./components/AddEmployee";
+import Login from "./components/Login/Login";
+import Logout from "./components/Logout/Logout";
+import LogoutConfirmation from "./components/Logout/LogoutConfirmation";
+import { useSelector } from "react-redux";
+import PrivateRoute from "./PrivateRoute";
+
 
 function App() {
+  const { user, isLoggedIn } = useSelector((state) => state.auth);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        {isLoggedIn && <NavbarHeader />}
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Login} />
+          <PrivateRoute exact path="/home" component={Home} />
+          <Route exact path="/changepwd" component={ChangePassword} />
+          <PrivateRoute path="/expenseEntries" component={AddExpenseSheet} />
+          <PrivateRoute path="/clients" component={Client} />
+          <PrivateRoute path="/projects" component={Project} />
+          <PrivateRoute
+            exact
+            path="/projectsummary"
+            component={ProjectSummary}
+          />
+          <PrivateRoute
+            path="/projectsummary/:pid"
+            component={ProjectSummary}
+          />
+          <PrivateRoute path="/approvals" component={Approvals} />
+          {isLoggedIn && user.role === "ADMIN" && (<PrivateRoute path="/addemployee" component={AddEmployee} />)}
+          <PrivateRoute path="/employees" component={Employees} />
+          <Route exact path="/logout" component={Logout} />
+          <Route
+            exact
+            path="/logoutconfirmation"
+            component={LogoutConfirmation}
+          />
+          <Route default component={Login} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
+
+
+
+
